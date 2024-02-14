df1e88c3960f50ab20cf98129099a088a77a5c46
imingyu <mingyuhisoft@163.com>
2024-02-14 20:59:15 +0800

    build: 优化小程序编译产物。优化所有的package.json

--- scripts/build.ts
+++ scripts/build.ts
@@ -27,6 +27,11 @@ const getCoreBuildOptions = (): RollupOptions[] => {
                 {
                     format: 'esm',
                     file: ROOT_DIR + '/packages/core/dist/esm/index.js'
+                },
+                {
+                    format: 'umd',
+                    file: ROOT_DIR + '/packages/core/dist/esm/index.js',
+                    name: 'CrossVirtualList'
                 }
             ]
         },
@@ -41,80 +46,140 @@ const getCoreBuildOptions = (): RollupOptions[] => {
                 {
                     format: 'esm',
                     file: ROOT_DIR + '/packages/core/dist/esm/index.d.ts'
-                }
-            ]
-        }
-    ];
-};
-
-const getMpWxBuildOptions = (): [RollupOptions, () => Promise<any>] => {
-    return [
-        {
-            input: {
-                'components/regular/index': ROOT_DIR + '/packages/mp-wx/src/components/regular/index.ts',
-                'components/dynamic/index': ROOT_DIR + '/packages/mp-wx/src/components/dynamic/index.ts'
-            },
-            plugins: [
-                ...getDefaultRollupPlugins(),
-                typescript({
-                    tsconfig: ROOT_DIR + '/packages/mp-wx/tsconfig.json',
-                    // eslint-disable-next-line @typescript-eslint/no-require-imports
-                    typescript: require('typescript')
-                })
-            ],
-            output: [
-                {
-                    format: 'cjs',
-                    dir: ROOT_DIR + '/packages/mp-wx/dist/cjs',
-                    chunkFileNames: '[name].js',
-                    manualChunks: (id) => {
-                        if (id.includes('node_modules')) {
-                            return 'vender';
-                        }
-                        if (!id.includes('components')) {
-                            return 'common';
-                        }
-                    }
                 },
                 {
-                    format: 'esm',
-                    dir: ROOT_DIR + '/packages/mp-wx/dist/esm',
-                    chunkFileNames: '[name].js',
-                    manualChunks: (id) => {
-                        if (id.includes('node_modules')) {
-                            return 'vender';
-                        }
-                        if (!id.includes('components')) {
-                            return 'common';
-                        }
-                    }
+                    format: 'umd',
+                    file: ROOT_DIR + '/packages/core/dist/umd/index.d.ts',
+                    name: 'CrossVirtualList'
                 }
             ]
-        },
-        () => {
-            return Promise.all([
-                compilerMpResource(ROOT_DIR + '/packages/mp-wx/src', ROOT_DIR + '/packages/mp-wx/dist/cjs'),
-                compilerMpResource(ROOT_DIR + '/packages/mp-wx/src', ROOT_DIR + '/packages/mp-wx/dist/esm')
-            ]).then(() => {
-                rimraf.sync(ROOT_DIR + '/packages/mp-wx/examples/vl-dist');
-                return copyPromise(
-                    ROOT_DIR + '/packages/mp-wx/dist/esm/**/*.*',
-                    ROOT_DIR + '/packages/mp-wx/examples/vl-dist'
-                );
-            });
         }
     ];
 };
 
-export const buildPackage = (name: 'core' | 'mp-wx' | 'all') => {
+const getTypesBuildOptions = (): RollupOptions => {
+    return {
+        input: ROOT_DIR + '/packages/types/src/index.ts',
+        plugins: [dts()],
+        output: [
+            {
+                format: 'esm',
+                file: ROOT_DIR + '/packages/types/dist/index.d.ts'
+            }
+        ]
+    };
+};
+
+const getMpWxBuildOptions = (): Array<[RollupOptions, () => Promise<any>]> => {
+    return [
+        [
+            {
+                external: [/@cross-virtual-list\//, 'typescript-mp-component'],
+                input: {
+                    'components/regular/index': ROOT_DIR + '/packages/mp-wx/src/components/regular/index.ts',
+                    'components/dynamic/index': ROOT_DIR + '/packages/mp-wx/src/components/dynamic/index.ts'
+                },
+                plugins: [
+                    ...getDefaultRollupPlugins(),
+                    typescript({
+                        tsconfig: ROOT_DIR + '/packages/mp-wx/tsconfig.json',
+                        // eslint-disable-next-line @typescript-eslint/no-require-imports
+                        typescript: require('typescript')
+                    })
+                ],
+                output: [
+                    {
+                        format: 'cjs',
+                        dir: ROOT_DIR + '/packages/mp-wx/dist/cjs',
+                        chunkFileNames: '[name].js',
+                        manualChunks: (id) => {
+                            if (id.includes('node_modules')) {
+                                return 'vender';
+                            }
+                            if (!id.includes('components')) {
+                                return 'common';
+                            }
+                        }
+                    },
+                    {
+                        format: 'esm',
+                        dir: ROOT_DIR + '/packages/mp-wx/dist/esm',
+                        chunkFileNames: '[name].js',
+                        manualChunks: (id) => {
+                            if (id.includes('node_modules')) {
+                                return 'vender';
+                            }
+                            if (!id.includes('components')) {
+                                return 'common';
+                            }
+                        }
+                    }
+                ]
+            },
+            () => {
+                return Promise.all([
+                    compilerMpResource(ROOT_DIR + '/packages/mp-wx/src', ROOT_DIR + '/packages/mp-wx/dist/cjs'),
+                    compilerMpResource(ROOT_DIR + '/packages/mp-wx/src', ROOT_DIR + '/packages/mp-wx/dist/esm')
+                ]);
+            }
+        ],
+        [
+            {
+                input: {
+                    'components/regular/index': ROOT_DIR + '/packages/mp-wx/src/components/regular/index.ts',
+                    'components/dynamic/index': ROOT_DIR + '/packages/mp-wx/src/components/dynamic/index.ts'
+                },
+                plugins: [
+                    ...getDefaultRollupPlugins(),
+                    typescript({
+                        tsconfig: ROOT_DIR + '/packages/mp-wx/tsconfig.json',
+                        // eslint-disable-next-line @typescript-eslint/no-require-imports
+                        typescript: require('typescript')
+                    })
+                ],
+                output: [
+                    {
+                        format: 'esm',
+                        dir: ROOT_DIR + '/packages/mp-wx/dist/esm-full',
+                        chunkFileNames: '[name].js',
+                        manualChunks: (id) => {
+                            if (id.includes('node_modules')) {
+                                return 'vender';
+                            }
+                            if (!id.includes('components')) {
+                                return 'common';
+                            }
+                        }
+                    }
+                ]
+            },
+            () => {
+                return Promise.all([
+                    compilerMpResource(ROOT_DIR + '/packages/mp-wx/src', ROOT_DIR + '/packages/mp-wx/dist/esm-full')
+                ]).then(() => {
+                    rimraf.sync(ROOT_DIR + '/packages/mp-wx/examples/vl-dist');
+                    return copyPromise(
+                        ROOT_DIR + '/packages/mp-wx/dist/esm-full/**/*.*',
+                        ROOT_DIR + '/packages/mp-wx/examples/vl-dist'
+                    );
+                });
+            }
+        ]
+    ];
+};
+
+export const buildPackage = (name: 'types' | 'core' | 'mp-wx' | 'all') => {
+    if (name === 'types') {
+        return fireRollup([getTypesBuildOptions()]);
+    }
     if (name === 'core') {
         return fireRollup(getCoreBuildOptions());
     }
     if (name === 'mp-wx') {
-        return fireRollup([getMpWxBuildOptions()]);
+        return fireRollup(getMpWxBuildOptions());
     }
     return Promise.all(
-        ['core', 'mp-wx'].map((name) => {
+        ['types', 'core', 'mp-wx'].map((name) => {
             return buildPackage(name as any);
         })
     );
